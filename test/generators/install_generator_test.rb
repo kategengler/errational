e13768d64a308f8606ebc15e0620f98fa9bf751b
Brian Olore <brian@olore.net>
2011-07-28 16:44:37 -0700

    remove some of the dumps to stderr... one left

--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -4,7 +4,10 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   tests Errational::Generators::InstallGenerator
   destination File.expand_path("../../tmp", __FILE__)
 
-  setup :prepare_destination
+  def setup
+    $stderr.stubs(:write)
+    prepare_destination
+  end
 
   def test_assert_all_files_are_properly_created
     run_generator ["RailsApp"]
